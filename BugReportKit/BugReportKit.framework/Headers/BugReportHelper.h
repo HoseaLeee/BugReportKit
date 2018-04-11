5e4d229f035a16cc35490608be4d99ec937a9888
Hosea Lee <co.hosea.lee@gmail.com>
2018-04-11 09:10:20 +0800

    添加方法注释

--- BugReportKit/BugReportKit.framework/Headers/BugReportHelper.h
+++ BugReportKit/BugReportKit.framework/Headers/BugReportHelper.h
@@ -12,7 +12,13 @@
 
 - (instancetype)init NS_UNAVAILABLE;
 + (instancetype)new NS_UNAVAILABLE;
-    
+  
+
+/**
+ 初始化Bugly,使用默认BuglyConfig
+
+ @param appId 注册Bugly分配的应用唯一标识
+ */
 + (void)startWithAppId:(NSString * )appId;
 
 @end
